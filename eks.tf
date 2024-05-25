743a3f3fbdfaba1764346a551a32c5b319b247eb
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-05-25 18:36:45 -0500

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -4,8 +4,9 @@ module "eks" {
   cluster_version = "1.26"
   subnet_ids     = [aws_subnet.private[0].id, aws_subnet.private[1].id]
   vpc_id          = aws_vpc.main.id
-  node_groups = {
-    eks_nodes = {
+  eks_managed_node_groups = {
+
+    node_group = {
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
