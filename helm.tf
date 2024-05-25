fd0d46ff1e5de28bd9db31551e5eb0cde55cace5
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-05-25 17:55:00 -0500

    Create helm.tf

---
+++ helm.tf
@@ -0,0 +1,31 @@
+resource "helm_release" "alb_ingress_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "region"
+    value = "us-east-1"
+  }
+
+  set {
+    name  = "vpcId"
+    value = aws_vpc.main.id
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+}
