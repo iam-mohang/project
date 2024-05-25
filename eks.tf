6f72fe8b2d6ad52b671e34d8556fe603b5316d56
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-05-25 17:48:25 -0500

    Create eks.tf

---
+++ eks.tf
@@ -0,0 +1,16 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "mohan-pro"
+  cluster_version = "1.26"
+  subnets         = [aws_subnet.private[0].id, aws_subnet.private[1].id]
+  vpc_id          = aws_vpc.main.id
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_type = "t3.medium"
+    }
+  }
+}
