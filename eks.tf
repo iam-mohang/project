094dfc62c53f7d522898fe4a4967fb155d45b2b6
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-05-25 18:48:18 -0500

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -3,16 +3,23 @@ module "eks" {
   version         = "18.30.0"  # Specify the appropriate version here
   cluster_name    = "mohan-pro"
   cluster_version = "1.26"
-  subnet_ids      = [aws_subnet.private[0].id, aws_subnet.private[1].id]
+  subnets         = [aws_subnet.private[0].id, aws_subnet.private[1].id]
   vpc_id          = aws_vpc.main.id
+}
 
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
+module "eks_node_group" {
+  source = "terraform-aws-modules/eks-node-group/aws"
+  version = "5.0.0"  # Specify the appropriate version here
 
-      instance_type = "t3.medium"
-    }
+  cluster_name    = module.eks.cluster_id
+  cluster_version = "1.26"
+  node_group_name = "eks-nodes"
+  node_group = {
+    desired_capacity = 2
+    max_capacity     = 3
+    min_capacity     = 1
+
+    instance_type = "t3.medium"
   }
+  subnets = [aws_subnet.private[0].id, aws_subnet.private[1].id]
 }
