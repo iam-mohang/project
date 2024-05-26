30f938f67ba596c638a8191ebeb11aff908c3d64
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-05-25 22:55:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,3 +57,22 @@ resource "aws_route_table_association" "public" {
   subnet_id = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public[0].id
+  tags = {
+    Name = "main-nat-gw"
+  }
+}
+
+resource "aws_route" "private_route" {
+  count                  = 2
+  route_table_id         = aws_subnet.private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
+}
