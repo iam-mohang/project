c00366cee62e3f4acaa4ce2c0281458db9ea68a5
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-05-25 19:09:33 -0500

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -9,8 +9,8 @@ module "eks" {
   eks_managed_node_groups = {
     eks_nodes = {
       desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
+      max_capacity     = 4
+      min_capacity     = 2
 
       instance_type = "t3.medium"
     }
