b27da150ada6622a9af4c06f4e738c30d7499801
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-05-25 19:01:29 -0500

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version         = "18.30.0"
   cluster_name    = "mohan-pro"
   cluster_version = "1.26"
-  subnets         = [aws_subnet.private[0].id, aws_subnet.private[1].id]
+  subnet_ids      = [aws_subnet.private[0].id, aws_subnet.private[1].id]
   vpc_id          = aws_vpc.main.id
 
   eks_managed_node_groups = {
