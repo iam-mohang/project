3d925492c006f5854f33b60b4e86692a33783ce8
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-05-26 21:22:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,9 @@ data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
   enable_dns_support = true
   enable_dns_hostnames = true
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  
   tags = {
     Name = "main-vpc"
   }
